b8739a2c5dbc64ade4fc3a667faa8e163f0a8793
leannaxx <94223093+leannaxx@users.noreply.github.com>
2023-10-16 19:13:18 -0700

    adding postgres extension+ modifications of the
    added pg + knexfile modified so it can find the db

---
+++ ActUFC/knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      user: 'postgres',
+      password: '22042003',
+      database: 'projectActUFC',
+    }
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      database: 'projectActUFC',
+      user:     'postgres',
+      password: '22042003'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: 'projectActUFC',
+      user:     'postgres',
+      password: '22042003'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
